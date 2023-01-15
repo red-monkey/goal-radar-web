f8e52cb83d2c756261577c31be9f5de59d3410fd
alilazim <ali.lazim@cr34.com>
2023-01-15 12:04:35 -0500

    Send email fixing in Contact.tsx

--- src/screens/contact/Contact.tsx
+++ src/screens/contact/Contact.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import axios from 'axios'
 import logo from "../../components/images/GoalRadarLogo.png";
 
-function Contact() {
+const Contact=()=> {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  
+  const contactUsHandler=async(to:string,subject:string,text:string)=> { 
+    await axios.get(
+      `http://goalradar.com.tr:3000/v1/email/send?to=${to}&subject=${subject}&text=${text}`,
+    );
+  };
+  
+  const submitHandler = (e:any) => {
+    e.preventDefault()
+    console.log(name,email,message)
+    contactUsHandler("goalradardev@gmail.com", name + "-" + email, message)
+    alert("Mesaj basari ile gonderildi!")
+    setName('')
+    setEmail('')
+    setMessage('')
+  }
+
+  useEffect(()=>{},[submitHandler])
+
   return (
+
     <div className="contact-container">
       <div className="contact-title">GOAL RADAR’A ULAŞIN</div>
       <div className="contact-logo-container">
@@ -13,29 +37,35 @@ function Contact() {
           Tüm öneri ve şikayetleriniz için iletişim formunu doldurabilir ve
           bizlere ulaşabilirsiniz. En kısa sürede size dönüş sağlanacaktır.
         </p>
-        <form className="contact-form-container">
+        <form className="contact-form-container" onSubmit={submitHandler}>
           <label>
             <input
               type="text"
               name="name"
               placeholder="isim,Soyad:"
               className="form-label"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </label>
           <label>
             <input
-              type="text"
-              name="name"
+              type="email"
+              name="email"
               placeholder="E-posta Adresi:"
               className="form-label"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </label>
           <label>
             <input
               type="text"
-              name="name"
+              name="message"
               className="form-label"
               placeholder="Mesajınızı buraya yazabilirsiniz:"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             />
           </label>
           <input type="submit" value="Gönder" className="submitButton" />
